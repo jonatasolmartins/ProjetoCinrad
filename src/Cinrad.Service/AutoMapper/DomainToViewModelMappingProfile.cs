055b9a60de4f53522957636c812ff73fe125613c
Jonatas <jonatasolmartins@outlook.com>
2019-03-23 03:47:18 -0300

    Adicionando automapper ao projeto.

---
+++ src/Cinrad.Service/AutoMapper/DomainToViewModelMappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Cinrad.Core.Entity;
+using Cinrad.Service.ViewModels;
+
+namespace Cinrad.Service.AutoMapper
+{
+    public class DomainToViewModelMappingProfile : Profile
+    {
+        public DomainToViewModelMappingProfile()
+        {
+            CreateMap<Usuario, UsuarioViewModel>();
+            CreateMap<Cliente, ClienteViewModel>();
+            CreateMap<Transportadora, TransportadoraViewModel>();
+        }
+    }
+}
---
+++ src/Cinrad.Service/AutoMapper/ViewModelToDomainMappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Cinrad.Core.Entity;
+using Cinrad.Service.ViewModels;
+
+namespace Cinrad.Service.AutoMapper
+{
+    public class ViewModelToDomainMappingProfile : Profile
+    {
+        public ViewModelToDomainMappingProfile()
+        {
+            CreateMap<UsuarioViewModel, Usuario>();
+            CreateMap<ClienteViewModel, Cliente>();
+            CreateMap<TransportadoraViewModel, Transportadora>();
+        }
+    }
+}
