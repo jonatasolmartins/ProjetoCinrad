aed7d99352228d9d40a66dfa56bb7b24cdfc70ce
Jonatas <jonatasolmartins@outlook.com>
2019-03-28 13:30:32 -0300

    Adição de nova viewmodel.

---
+++ src/Cinrad.Service/ViewModels/ClienteTransportadoraViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Cinrad.Service.ViewModels
+{
+    public class ClienteTransportadoraViewModel
+    {
+        public Guid ClienteId { get; set; }
+
+        [Display(Name = "Cliente")]
+        public ClienteViewModel Cliente { get; set; }
+        public Guid TransportadoraId { get; set; }
+
+        [Display(Name = "Transportadora")]
+        public TransportadoraViewModel Transportadora { get; set; }
+
+    }
+}
