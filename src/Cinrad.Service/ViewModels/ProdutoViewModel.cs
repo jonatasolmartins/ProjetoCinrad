52bb22f0316fc73c9237828148be6539fbf5a2b8
Jonatas <jonatasolmartins@outlook.com>
2019-04-21 18:41:02 -0300

    Adicioando ViewModel Produto.

--- src/Cinrad.Service/ViewModels/ProdutoViewModel.cs
+++ src/Cinrad.Service/ViewModels/ProdutoViewModel.cs
@@ -1,6 +1,16 @@
-﻿namespace Cinrad.Service.ViewModels
+﻿using System;
+
+namespace Cinrad.Service.ViewModels
 {
     public class ProdutoViewModel
     {
+        public Guid Id { get; set; }
+        public Guid Codigo { get; set; }
+        public string Nome { get; set; }
+        public string Descrisao { get; set; }
+        public bool IsDadosObrigatorio { get; set; }
+        public Guid CodigoCor { get; set; }
+        public string UnidadeMedida { get; set; }
+        public string UnidadeVenda { get; set; }
     }
 }
